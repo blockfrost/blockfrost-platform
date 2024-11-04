1edee9b7e1f0415717d6edcf34805d91900e9aba
Vladimir Volek <volek.vladimir@gmail.com>
2024-11-05 00:41:07 +0100

    fix: lint

--- src/schema.rs
+++ src/schema.rs
@@ -31,7 +31,4 @@ diesel::table! {
 
 diesel::joinable!(requests -> users (user_id));
 
-diesel::allow_tables_to_appear_in_same_query!(
-    requests,
-    users,
-);
+diesel::allow_tables_to_appear_in_same_query!(requests, users,);
