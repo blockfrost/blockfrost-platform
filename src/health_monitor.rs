a59c06e39d08acb0791ce04ee387e2081d941159
Michal Rus <michal.rus@iohk.io>
2025-03-03 15:56:57 +0100

    chore: check that the chain is advancing in `health_monitor`
    Resolves #205

--- src/health_monitor.rs
+++ src/health_monitor.rs
@@ -21,6 +21,8 @@ impl HealthMonitor {
     }
 }
 
+const CHAIN_STALE_IF_OLDER_THAN: std::time::Duration = std::time::Duration::from_secs(5 * 60);
+
 pub async fn spawn(node: NodePool) -> HealthMonitor {
     // This initial state is never seen:
     let state_ = Arc::new(RwLock::new(HealthStatus {
@@ -34,13 +36,44 @@ pub async fn spawn(node: NodePool) -> HealthMonitor {
     let state_update = state_update_.clone();
 
     tokio::spawn(async move {
+        let mut last_chain_advancement = std::time::Instant::now();
+        let mut last_chain_block =
+            "0000000000000000000000000000000000000000000000000000000000000000".to_string();
+
         loop {
             let node_info: Result<NodeInfo, BlockfrostError> = async {
                 let mut node = node.get().await?;
                 node.sync_progress().await
             }
             .await;
-            let errors: Vec<BlockfrostError> = node_info.clone().err().into_iter().collect();
+
+            if let Ok(node_info) = node_info.as_ref() {
+                if last_chain_block != node_info.block {
+                    last_chain_block = node_info.block.clone();
+                    last_chain_advancement = std::time::Instant::now();
+                }
+            }
+
+            let chain_too_stale = {
+                let elapsed = last_chain_advancement.elapsed();
+                if elapsed > CHAIN_STALE_IF_OLDER_THAN {
+                    let err = format!(
+                        "Chain stuck at {}, has not seen updates in {:?}.",
+                        last_chain_block, elapsed
+                    );
+                    tracing::error!("{}", err);
+                    Some(BlockfrostError::internal_server_error(err))
+                } else {
+                    None
+                }
+            };
+
+            let errors: Vec<BlockfrostError> = node_info
+                .clone()
+                .err()
+                .into_iter()
+                .chain(chain_too_stale.into_iter())
+                .collect();
             let healthy = errors.is_empty();
             let node_info = node_info.ok();
 
