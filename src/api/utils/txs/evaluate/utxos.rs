5fae7b2664c31ac21ba18b9c8f1602e916e1415c
Hızır Sefa İrken <sefairken@gmail.com>
2025-05-05 20:05:44 +0300

    Implement tx submit evaluation via pallas

--- src/api/utils/txs/evaluate/utxos.rs
+++ src/api/utils/txs/evaluate/utxos.rs
@@ -1,6 +1,20 @@
-use crate::{BlockfrostError, api::ApiResult};
-use serde_json::Value;
+use crate::{BlockfrostError, NodePool, cbor::evaluate, common::validate_content_type};
+use axum::{Extension, Json, extract, response::IntoResponse};
+use hyper::HeaderMap;
 
-pub async fn route() -> ApiResult<Value> {
-    Err(BlockfrostError::not_found())
+use super::model::{TxEvaluationRequest, convert_eval_report};
+
+pub async fn route(
+    Extension(node): Extension<NodePool>,
+    headers: HeaderMap,
+    extract::Json(tx_request): extract::Json<TxEvaluationRequest>,
+) -> Result<impl IntoResponse, BlockfrostError> {
+    // Allow only application/json content type
+    validate_content_type(&headers, &["application/json"])?;
+
+    let pallas_report =
+        evaluate::evaluate_encoded_tx(node, &tx_request.cbor, tx_request.additional_utxo_set)
+            .await?;
+
+    Ok(Json(convert_eval_report(pallas_report)))
 }
