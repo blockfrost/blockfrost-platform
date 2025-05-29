a04df0fa046da91e95407c07f083cbfe441b2b2c
Vladimír Volek <volek.vladimir@gmail.com>
2025-05-29 15:29:47 +0200

    fix: last test

--- tests/common.rs
+++ tests/common.rs
@@ -41,7 +41,7 @@ pub fn test_config(icebreakers_config: Option<IcebreakersConfig>) -> Arc<Config>
         icebreakers_config,
         max_pool_connections: 10,
         network: Network::Preview,
-        no_metrics: true,
+        no_metrics: false,
         custom_genesis_config: None,
     };
 
