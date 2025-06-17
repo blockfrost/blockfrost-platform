6eac521476f87cc41ddb3a774d16597cf4c82a5c
Vladimír Volek <volek.vladimir@gmail.com>
2025-06-17 22:36:54 +0200

    fix: metrics

--- src/server.rs
+++ src/server.rs
@@ -16,6 +16,7 @@ use metrics::{setup_metrics_recorder, spawn_process_collector};
 use routes::{hidden::get_hidden_api_routes, nest_routes, regular::get_regular_api_routes};
 use state::{ApiPrefix, AppState};
 use std::sync::Arc;
+use tower::Layer;
 use tower_http::normalize_path::NormalizePathLayer;
 use uuid::Uuid;
 
@@ -70,10 +71,9 @@ pub async fn build(
     };
 
     // Add layers
-    let app = {
+    let inner = {
         let mut routes = api_routes
             .with_state(app_state.clone())
-            .layer(NormalizePathLayer::trim_trailing_slash())
             .layer(Extension(health_monitor.clone()))
             .layer(Extension(node_conn_pool.clone()))
             .layer(from_fn(error_middleware))
@@ -85,6 +85,9 @@ pub async fn build(
 
         routes
     };
+
+    let inner = NormalizePathLayer::trim_trailing_slash().layer(inner);
+    let app = Router::new().nest_service("/", inner);
 
     Ok((
         app,
