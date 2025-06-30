39770c1640984dc609f9ff858881f627c37be8b7
Michal Rus <michal.rus@iohk.io>
2025-06-30 14:25:04 +0200

    fix: `aarch64-linux` compilation of Pallas’ `phase2`

--- crates/build_utils/src/features.rs
+++ crates/build_utils/src/features.rs
@@ -1,16 +1,16 @@
 pub fn evaluate(target_os: &str, target_arch: &str) {
-        println!("cargo::rustc-check-cfg=cfg(evaluate)");
+    println!("cargo::rustc-check-cfg=cfg(evaluate)");
 
-        if (target_os == "linux" && target_arch == "aarch64") || target_os == "windows" {
-            println!(
-                "cargo:warning=Skipping 'evaluate' cfg for {}-{}",
-                target_os, target_arch
-            );
-        } else {
-            println!(
-                "cargo:warning=Going to build with 'evaluate' cfg for {}-{}",
-                target_os, target_arch
-            );
-            println!("cargo:rustc-cfg=evaluate");
-        }
+    if target_os == "windows" {
+        println!(
+            "cargo:warning=Skipping 'evaluate' cfg for {}-{}",
+            target_os, target_arch
+        );
+    } else {
+        println!(
+            "cargo:warning=Going to build with 'evaluate' cfg for {}-{}",
+            target_os, target_arch
+        );
+        println!("cargo:rustc-cfg=evaluate");
     }
+}
