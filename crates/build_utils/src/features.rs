11cf2cf95b74e842554a7b7d6e9a5998f9ad6b5f
Michal Rus <michal.rus@iohk.io>
2025-07-03 15:16:10 +0200

    chore: use the native `aarch64-linux` build of `testgen-hs` (with extra tx-gen features)

--- crates/build_utils/src/features.rs
+++ crates/build_utils/src/features.rs
@@ -2,14 +2,10 @@ pub fn evaluate(target_os: &str, target_arch: &str) {
     println!("cargo::rustc-check-cfg=cfg(evaluate)");
 
     if target_os == "windows" {
-        println!(
-            "cargo:warning=Skipping 'evaluate' cfg for {}-{}",
-            target_os, target_arch
-        );
+        println!("cargo:warning=Skipping 'evaluate' cfg for {target_os}-{target_arch}");
     } else {
         println!(
-            "cargo:warning=Going to build with 'evaluate' cfg for {}-{}",
-            target_os, target_arch
+            "cargo:warning=Going to build with 'evaluate' cfg for {target_os}-{target_arch}"
         );
         println!("cargo:rustc-cfg=evaluate");
     }
