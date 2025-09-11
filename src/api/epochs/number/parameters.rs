cbd272054e24acc2881c80ba45d88cd8b422a82f
Vladimír Volek <volek.vladimir@gmail.com>
2025-09-11 16:46:16 +0200

    feat: epoch number validation

--- src/api/epochs/number/parameters.rs
+++ src/api/epochs/number/parameters.rs
@@ -1,13 +1,17 @@
 use crate::server::state::AppState;
 use api_provider::types::EpochsParamResponse;
 use axum::extract::{Path, State};
-use common::{epochs::EpochsPath, types::ApiResult};
+use common::{
+    epochs::{EpochData, EpochsPath},
+    types::ApiResult,
+};
 
 pub async fn route(
     State(state): State<AppState>,
     Path(epochs_path): Path<EpochsPath>,
 ) -> ApiResult<EpochsParamResponse> {
+    let epoch_data = EpochData::from_path(epochs_path.epoch_number, &state.config.network)?;
     let dolos = state.get_dolos()?;
 
-    dolos.epochs().parameters(&epochs_path.epoch_number).await
+    dolos.epochs().parameters(&epoch_data.epoch_number).await
 }
