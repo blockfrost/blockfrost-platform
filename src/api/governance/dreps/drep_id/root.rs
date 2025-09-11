80559cb0c4f6421d6e9c8cf6064a20e6758621f4
Vladimír Volek <volek.vladimir@gmail.com>
2025-09-11 15:02:56 +0200

    fix: dreps

--- src/api/governance/dreps/drep_id/root.rs
+++ src/api/governance/dreps/drep_id/root.rs
@@ -1,6 +1,13 @@
-use crate::{BlockfrostError, api::ApiResult};
+use crate::server::state::AppState;
 use api_provider::types::DrepsSingleResponse;
+use axum::extract::{Path, State};
+use common::{dreps::DrepsPath, types::ApiResult};
 
-pub async fn route() -> ApiResult<DrepsSingleResponse> {
-    Err(BlockfrostError::not_found())
+pub async fn route(
+    Path(drep_path): Path<DrepsPath>,
+    State(state): State<AppState>,
+) -> ApiResult<DrepsSingleResponse> {
+    let dolos = state.get_dolos()?;
+
+    dolos.governance().drep(&drep_path.drep_id).await
 }
