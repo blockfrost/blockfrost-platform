b6cb5b45e1a866511ec63782dc478a0c37868b91
Vladimír Volek <volek.vladimir@gmail.com>
2025-09-16 17:31:25 +0200

    fix: try new reporter

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     cardano-node.flake = false; # otherwise, +2k dependencies we don’t really use
     dolos.url = "github:txpipe/dolos/v0.32.0";
     dolos.flake = false;
-    blockfrost-tests.url = "github:blockfrost/blockfrost-tests";
+    blockfrost-tests.url = "github:blockfrost/blockfrost-tests/d50edcf5c221d76590a7aec749b618810b009930";
     blockfrost-tests.flake = false;
     mithril.url = "github:input-output-hk/mithril/2524.0";
     testgen-hs.url = "github:input-output-hk/testgen-hs/10.4.1.1"; # make sure it follows cardano-node
