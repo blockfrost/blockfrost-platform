c27f63e8775c1d08457369c9c05ed9670e6b9724
Michal Rus <michal.rus@iohk.io>
2026-03-02 14:08:57 +0100

    fix(ci): the Windows build of `blockfrost-gateway.exe` _including_ `libpq`

--- crates/gateway/src/models.rs
+++ crates/gateway/src/models.rs
@@ -1,13 +1,10 @@
 use chrono::NaiveDateTime;
+use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[cfg(not(target_os = "windows"))]
-use diesel::prelude::*;
-
-#[derive(Deserialize, Serialize, Debug)]
-#[cfg_attr(not(target_os = "windows"), derive(Queryable, Selectable, Insertable))]
-#[cfg_attr(not(target_os = "windows"), diesel(table_name = crate::schema::requests))]
-#[cfg_attr(not(target_os = "windows"), diesel(check_for_backend(diesel::pg::Pg)))]
+#[derive(Queryable, Selectable, Insertable, Deserialize, Serialize, Debug)]
+#[diesel(table_name = crate::schema::requests)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Request {
     pub id: i32,
     pub route: String,
@@ -17,10 +14,9 @@ pub struct Request {
     pub reward_address: String,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-#[cfg_attr(not(target_os = "windows"), derive(Selectable, Insertable))]
-#[cfg_attr(not(target_os = "windows"), diesel(table_name = crate::schema::requests))]
-#[cfg_attr(not(target_os = "windows"), diesel(check_for_backend(diesel::pg::Pg)))]
+#[derive(Selectable, Insertable, Deserialize, Serialize, Debug)]
+#[diesel(table_name = crate::schema::requests)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct RequestNewItem {
     pub route: String,
     pub user_id: i32,
@@ -31,10 +27,9 @@ pub struct RequestNewItem {
     pub asset_name: Option<String>,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-#[cfg_attr(not(target_os = "windows"), derive(Selectable, Insertable, Queryable))]
-#[cfg_attr(not(target_os = "windows"), diesel(table_name = crate::schema::users))]
-#[cfg_attr(not(target_os = "windows"), diesel(check_for_backend(diesel::pg::Pg)))]
+#[derive(Selectable, Insertable, Queryable, Deserialize, Serialize, Debug)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
     pub id: i32,
     pub created_at: NaiveDateTime,
