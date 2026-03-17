426e7c1c5b21256fff8f9f141121d23553c6c3b4
Vladimír Volek <volek.vladimir@gmail.com>
2026-03-17 15:11:30 +0100

    fix: genesis unit tests

--- crates/common/src/genesis.rs
+++ crates/common/src/genesis.rs
@@ -130,7 +130,7 @@ mod tests {
     #[rstest]
     #[case(764_824_073, 1_506_203_091)]
     #[case(1, 1_654_041_600)]
-    #[case(2, 1_666_692_000)]
+    #[case(2, 1_666_656_000)]
     fn test_by_magic_returns_correct_start(#[case] magic: i32, #[case] expected_start: i32) {
         let registry = genesis();
         let genesis = registry.by_magic(magic as u64);
