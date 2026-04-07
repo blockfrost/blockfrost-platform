ab937da359a479748e078683d533366f3db0681c
Michal Rus <michal.rus@iohk.io>
2026-04-07 17:53:27 +0200

    chore(dolos): update to 1.0.3

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       flake = false; # otherwise, +2k dependencies we don’t really use
     };
     dolos = {
-      url = "github:txpipe/dolos/v1.0.0-rc.12";
+      url = "github:txpipe/dolos/v1.0.3";
       flake = false;
     };
     blockfrost-tests = {
